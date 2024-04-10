98f93c8d9b8d8eaf4931c07cdb8e55687c115ef5
Srikanth V M <srikanthvm15@gmail.com>
2024-04-10 22:18:48 +0530

    new

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the ImageViews by their IDs
+        val imageButton1: View = findViewById(R.id.imageButton2)
+        val imageButton2: View = findViewById(R.id.imageButton1)
+
+        // Set click listeners for the ImageViews
+        imageButton1.setOnClickListener {
+            // Handle redirection for button 1
+            startActivity(Intent(this, woods::class.java))
+        }
+
+        imageButton2.setOnClickListener {
+            // Handle redirection for button 2
+            startActivity(Intent(this, laminates::class.java))
+        }
     }
 }
